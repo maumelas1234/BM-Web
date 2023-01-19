52cb560b0d4a2bfd1f1b0d840abb4884210429a4
bartlomiej <bartlomiejmazur24@gmail.com>
2023-01-19 11:04:28 +0100

    feat: create Camera class

---
+++ Experience/Camera.js
@@ -0,0 +1,12 @@
+import Experience from "./Experience.js"
+
+export default class Camera{
+  constructor(){
+    this.experience = new Experience();
+    this.sizes = this.experience.sizes;
+    this.scene = this.experience.scene;
+    this.canvas = this.experience.canvas;
+    
+    
+  }
+}

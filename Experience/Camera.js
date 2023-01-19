2aed772a3c953a2a35ecfdbe995ba54ab0e57a9f
bartlomiej <bartlomiejmazur24@gmail.com>
2023-01-19 11:22:38 +0100

    create two cameras for scene

--- Experience/Camera.js
+++ Experience/Camera.js
@@ -1,3 +1,4 @@
+import * as THREE from "three"
 import Experience from "./Experience.js"
 
 export default class Camera{
@@ -6,7 +7,27 @@ export default class Camera{
     this.sizes = this.experience.sizes;
     this.scene = this.experience.scene;
     this.canvas = this.experience.canvas;
-    
-    
+
+    this.createPerspectiveCamera();
+    this.createOrthographicCamera();
   }
+
+  createPerspectiveCamera(){
+    this.perspectiveCamera = new THREE.PerspectiveCamera(35, this.sizes.aspect, 0.1, 1000);
+    this.scene.add(this.perspectiveCamera);
+  }
+
+  createOrthographicCamera(){
+    this.frustrum = 5;
+    this.orthographicCamera = new THREE.OrthographicCamera(
+      (-this.sizes.aspect * this.sizes.frustrum)/2,
+      (this.sizes.aspect * this.sizes.frustrum)/2,
+      this.sizes.frustrum/2,
+      -this.sizes.frustrum/2,
+      -100,
+      100
+      );
+    this.scene.add(this.orthographicCamera);
+  }
+  
 }

011cda0fb54d02d5e14f3596d96a30e941b73e94
bartlomiej <bartlomiejmazur24@gmail.com>
2023-01-19 13:44:50 +0100

    feat:import obj and obj initialization: Scene, Time, Sizes, Camera, Renderer. Add event emitter: resize (Sizes), update (Time).

--- Experience/Experience.js
+++ Experience/Experience.js
@@ -1,4 +1,9 @@
 import * as THREE from "three";
+import Sizes from "./Utils/Sizes.js"
+import Time from "./Utils/Time.js";
+
+import Camera from "./Camera.js";
+import Renderer from "./Renderer.js";
 
 export default class Experience{
   static instance
@@ -8,7 +13,28 @@ export default class Experience{
     }
     Experience.instance = this
     this.canvas = canvas
-    
-    
+    this.scene = new THREE.Scene();
+    this.time = new Time();
+    this.sizes = new Sizes();
+    this.camera = new Camera();
+    this.renderer = new Renderer();
+    //event emitter sizes.resize()
+    this.sizes.on("resize", ()=>{
+      this.resize();
+    })
+    //event emitter time.update()
+    this.time.on("update", ()=>{
+      this.update();
+    })
+  }
+//update browser window when resizing it
+  resize(){
+    this.camera.resize();
+    this.renderer.resize();
+  }
+//update over time
+  update(){
+    this.camera.update();
+    this.renderer.update();
   }
 }

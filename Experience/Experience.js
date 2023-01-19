9f0aae5f5e7c767cd11b9144fb3ffef89f1ce664
bartlomiej <bartlomiejmazur24@gmail.com>
2023-01-19 10:35:23 +0100

    feat: create parent class.

---
+++ Experience/Experience.js
@@ -0,0 +1,14 @@
+import * as THREE from "three";
+
+export default class Experience{
+  static instance
+  constructor(canvas){
+    if(Experience.instance){
+      return Experience.instance
+    }
+    Experience.instance = this
+    this.canvas = canvas
+    
+    
+  }
+}

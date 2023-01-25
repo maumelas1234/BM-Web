ecb9dd8c557aa12da8a1d03bdd33f302d675544f
bartlomiej <bartlomiejmazur24@gmail.com>
2023-01-25 18:19:50 +0100

    Remove mouse move detection (onMouseMove)

--- Experience/World/Room.js
+++ Experience/World/Room.js
@@ -1,70 +1,43 @@
-import * as THREE from "three"
-import GSAP from "gsap"
-import Experience from "../Experience.js"
+import * as THREE from "three";
+import GSAP from "gsap";
+import Experience from "../Experience.js";
 
-export default class Room{
-  constructor(){
-  this.experience = new Experience();
-  this.scene = this.experience.scene;
-  this.resources = this.experience.resources;
-  this.room = this.resources.items.room;
-  this.actualRoom = this.room.scene;
-  this.camera = this.experience.camera.perspectiveCamera
+export default class Room {
+  constructor() {
+    this.experience = new Experience();
+    this.scene = this.experience.scene;
+    this.resources = this.experience.resources;
+    this.room = this.resources.items.room;
+    this.actualRoom = this.room.scene;
+    this.camera = this.experience.camera.perspectiveCamera;
 
-  this.lerp = {
-    current: 0,
-    target: 0,
-    ease: 0.1,
-  };
-  
-  this.setModel();
-  this.onMouseMove();
+    this.setModel();
   }
 
-  setModel(){
+  setModel() {
     console.log(this.room);
-    
-    this.actualRoom.children.forEach((child) =>{
+
+    this.actualRoom.children.forEach((child) => {
       child.castShadow = true;
       child.receiveShadow = true;
 
-      if(child instanceof THREE.Group){
+      if (child instanceof THREE.Group) {
         child.children.forEach((groupchild) => {
           groupchild.castShadow = true;
           groupchild.receiveShadow = true;
-        })
+        });
       }
-      if(child.name === "Screen" || child.name === "Screen_left" || child.name === "Screen_right"){
+      if (child.name === "Screen") {
         child.material = new THREE.MeshBasicMaterial({
           map: this.resources.items.screen,
-          
-        })
+        });
       }
-    })
+    });
     this.scene.add(this.actualRoom);
     this.actualRoom.scale.set(0.1, 0.1, 0.1);
-    
   }
 
-  onMouseMove(){
-    window.addEventListener("mousemove", (e) =>{
-      this.rotation = (-(e.clientX - window.innerWidth / 2)/2) / window.innerWidth;
-      this.lerp.target = this.rotation * 0.4;
-      
-      
-    })
-  }
+  resize() {}
 
-  resize(){}
-
-  update(){
-    this.lerp.current = GSAP.utils.interpolate(
-      this.lerp.current,
-      this.lerp.target,
-      this.lerp.ease
-    );
-    
-    
-    this.camera.rotation.y = this.lerp.current
-  }
+  update() {}
 }

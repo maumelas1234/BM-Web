1e497397269c56284d1b1dccd946142cc4df819a
bartlomiej <bartlomiejmazur24@gmail.com>
2023-01-25 18:02:44 +0100

    Remove setOrbit and code formatting

--- Experience/Camera.js
+++ Experience/Camera.js
@@ -1,36 +1,31 @@
-import * as THREE from "three"
-import Experience from "./Experience.js"
+import * as THREE from "three";
+import Experience from "./Experience.js";
 
-
-
-export default class Camera{
-  constructor(){
+export default class Camera {
+  constructor() {
     this.experience = new Experience();
     this.sizes = this.experience.sizes;
     this.scene = this.experience.scene;
     this.canvas = this.experience.canvas;
-    //create two cameras for the scene
+    //create camera for scene
     this.createPerspectiveCamera();
-    
-    
   }
 
-  createPerspectiveCamera(){
-    this.perspectiveCamera = new THREE.PerspectiveCamera(35, this.sizes.aspect, 0.1, 1000);
+  createPerspectiveCamera() {
+    this.perspectiveCamera = new THREE.PerspectiveCamera(
+      35,
+      this.sizes.aspect,
+      0.1,
+      1000
+    );
     this.scene.add(this.perspectiveCamera);
-    
-   
   }
 
-  resize(){
+  resize() {
     //Updating Perspective Camera on Resize
     this.perspectiveCamera.aspect = this.sizes.aspect;
     this.perspectiveCamera.updateProjectionMatrix();
-    this.TestCamera.aspect = this.sizes.aspect;
-    this.TestCamera.updateProjectionMatrix();
   }
 
-  update(){
-  }
-  
+  update() {}
 }

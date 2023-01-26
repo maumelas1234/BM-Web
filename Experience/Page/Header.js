c951381d5aadd61324e56e0fa52fb743ea732485
bartlomiej <bartlomiejmazur24@gmail.com>
2023-01-26 17:11:00 +0100

    feat: Service nav in header

---
+++ Experience/Page/Header.js
@@ -0,0 +1,41 @@
+export default class Header {
+  constructor() {
+    this.navMenu = document.querySelector(".nav__menu");
+    this.navToggle = document.querySelector(".nav__toggle");
+    this.navClose = document.querySelector(".nav__close");
+    this.navLink = document.querySelectorAll(".nav__link");
+
+    this.showMenu();
+    this.menuHidden();
+    this.removeMenuMobile();
+  }
+
+  showMenu() {
+    /*===== MENU SHOW =====*/
+    /* Validate if constant exists */
+    if (this.navToggle) {
+      this.navToggle.addEventListener("click", () => {
+        this.navMenu.classList.add("show-menu");
+      });
+    }
+  }
+
+  menuHidden() {
+    /*===== MENU HIDDEN =====*/
+    /* Validate if constant exists */
+    if (this.navClose) {
+      this.navClose.addEventListener("click", () => {
+        this.navMenu.classList.remove("show-menu");
+      });
+    }
+  }
+
+  removeMenuMobile() {
+    /*=============== REMOVE MENU MOBILE ===============*/
+    const linkAction = () => {
+      // When we click on each nav__link, we remove the show-menu class
+      this.navMenu.classList.remove("show-menu");
+    };
+    this.navLink.forEach((n) => n.addEventListener("click", linkAction));
+  }
+}

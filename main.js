2843d35f9ba9bed18e5aadd53b380868ab21d40b
bartlomiej <bartlomiejmazur24@gmail.com>
2023-01-27 22:52:22 +0100

    import all style and create Header and Email

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-import './style.css'
-import Experience from './Experience/Experience'
-const experience = new Experience(document.querySelector(".experience-canvas"))
+import "./style/variables.css";
+import "./style.css";
+import "./style/header.css";
+import "./style/home.css";
+import "./style/skills.css";
+import "./style/qualification.css";
+import "./style/services.css";
+import "./style/projects.css";
+import "./style/contact.css";
+
+import Experience from "./Experience/Experience";
+
+import Header from "./Experience/Page/Header";
+import Email from "./Experience/Page/Email";
+
+const experience = new Experience(document.querySelector(".experience-canvas"));
+
+const header = new Header();
+const email = new Email();

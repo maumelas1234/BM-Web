a8d95b023e22946fea98525c4ec9fc1742b3b022
bartlomiej <bartlomiejmazur24@gmail.com>
2023-01-29 18:34:33 +0100

    feat: Import Preloader and create instance

--- Experience/Experience.js
+++ Experience/Experience.js
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import Sizes from "./Utils/Sizes.js"
+import Sizes from "./Utils/Sizes.js";
 import Time from "./Utils/Time.js";
 import Resources from "./Utils/Resources.js";
 import assets from "./Utils/assets.js";
@@ -9,15 +9,16 @@ import Theme from "./Theme.js";
 import Renderer from "./Renderer.js";
 
 import World from "./World/World.js";
+import Preloader from "./World/Preloader.js";
 
-export default class Experience{
-  static instance
-  constructor(canvas){
-    if(Experience.instance){
-      return Experience.instance
+export default class Experience {
+  static instance;
+  constructor(canvas) {
+    if (Experience.instance) {
+      return Experience.instance;
     }
-    Experience.instance = this
-    this.canvas = canvas
+    Experience.instance = this;
+    this.canvas = canvas;
     this.scene = new THREE.Scene();
     this.time = new Time();
     this.sizes = new Sizes();
@@ -26,25 +27,25 @@ export default class Experience{
     this.resources = new Resources(assets);
     this.theme = new Theme();
     this.world = new World();
+    this.preloader = new Preloader();
     //event emitter sizes.resize()
-    this.sizes.on("resize", ()=>{
+    this.sizes.on("resize", () => {
       this.resize();
-    })
+    });
     //event emitter time.update()
-    this.time.on("update", ()=>{
+    this.time.on("update", () => {
       this.update();
-    })
+    });
   }
-//update browser window when resizing it
-  resize(){
+  //update browser window when resizing it
+  resize() {
     this.camera.resize();
     this.renderer.resize();
   }
-//update over time
-  update(){
+  //update over time
+  update() {
     this.world.update();
     this.camera.update();
     this.renderer.update();
-  
   }
 }

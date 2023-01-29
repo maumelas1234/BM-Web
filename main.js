35249907e7ec1a42aeb2517d8da33629d2671e57
bartlomiej <bartlomiejmazur24@gmail.com>
2023-01-29 18:35:31 +0100

    Import all pages tool and import style

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import "./style/variables.css";
+import "/style/preloader.css";
 import "./style.css";
 import "./style/header.css";
 import "./style/home.css";
@@ -7,13 +8,22 @@ import "./style/qualification.css";
 import "./style/services.css";
 import "./style/projects.css";
 import "./style/contact.css";
+import "./style/footer.css";
+import "./style/scroll.css";
+import "./style/rate.css";
+import "./style/breakpoints.css";
 
 import Experience from "./Experience/Experience";
 
-import Header from "./Experience/Page/Header";
+import Nav from "./Experience/Page/Nav";
+// import Slider from "./Experience/Page/Slider";
 import Email from "./Experience/Page/Email";
+import Scroll from "./Experience/Page/Scroll";
+import RateModal from "./Experience/Page/RateModal";
 
 const experience = new Experience(document.querySelector(".experience-canvas"));
 
-const header = new Header();
+const nav = new Nav();
 const email = new Email();
+const scrool = new Scroll();
+const rateModal = new RateModal();

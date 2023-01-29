15126f3a6f1c8c44be71f6ae7a8994e30304df18
bartlomiej <bartlomiejmazur24@gmail.com>
2023-01-29 19:27:19 +0100

    merge style

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
-import "./style/variables.css";
-import "/style/preloader.css";
 import "./style.css";
-import "./style/header.css";
-import "./style/home.css";
-import "./style/skills.css";
-import "./style/qualification.css";
-import "./style/services.css";
-import "./style/projects.css";
-import "./style/contact.css";
-import "./style/footer.css";
-import "./style/scroll.css";
-import "./style/rate.css";
-import "./style/breakpoints.css";
 
 import Experience from "./Experience/Experience";
 
